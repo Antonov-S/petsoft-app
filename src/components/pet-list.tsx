15b3f7a2c30c8c057474aa1e4ea9d527cc85e68f
Antonov-S <anto1nov1@gmail.com>
2024-03-24 19:40:24 +0200

    Changing background color for active / selected pet by using cn() utility function for conditional classes

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -3,16 +3,22 @@
 import Image from "next/image";
 
 import { usePetContext } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
 
 export default function PetList() {
-  const { pets, handleChangeSelectedPetId } = usePetContext();
+  const { pets, selectedPetId, handleChangeSelectedPetId } = usePetContext();
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map(pet => (
         <li key={pet.id}>
           <button
             onChange={() => handleChangeSelectedPetId(pet.id)}
-            className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+            className={cn(
+              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+              {
+                "bg-[#EFF1F2]": pet.id === selectedPetId
+              }
+            )}
           >
             <Image
               src={pet.imageUrl}
