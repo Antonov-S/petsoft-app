69876e81db9e109a844a84c2598cd8ad67d81af7
Antonov-S <anto1nov1@gmail.com>
2024-04-12 21:20:17 +0300

    Finish the auth pages, including the auth form

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,21 @@
-export default function SignUp() {
-  return <div>SignUp page</div>;
+import Link from "next/link";
+
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+
+export default function Signup() {
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Sign Up</H1>
+
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Log In
+        </Link>
+      </p>
+    </main>
+  );
 }
