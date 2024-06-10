cfc08cb3c2f10f35d9e97e320a7a3dce0dac2a16
Antonov-S <anto1nov1@gmail.com>
2024-06-11 01:52:23 +0300

    Create the payment page

---
+++ src/app/(auth)/payment/page.tsx
@@ -0,0 +1,13 @@
+import H1 from "@/components/h1";
+import { Button } from "@/components/ui/button";
+
+function Page() {
+  return (
+    <main className="flex flex-col items-center space-y-10">
+      <H1>PetSoft access requires payment</H1>
+      <Button>Buy lifetime access for $19.99</Button>
+    </main>
+  );
+}
+
+export default Page;
